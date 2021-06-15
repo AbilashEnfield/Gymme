7d3d34aa978fc425c7d1e877f2b0fd500a974ec3
Abilash <abilashenfield@gmail.com>
2021-06-15 16:48:17 +0530

    key

--- Gymme/settings.py
+++ Gymme/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = "django-insecure-^4^o9$+p%y8q_gjah=@*#rgq!fgbgspe3ttv&qrz*g(b=&1g@g"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
